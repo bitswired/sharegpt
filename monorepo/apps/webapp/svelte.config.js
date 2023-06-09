b72343a03a3e84122a0fbe513292b1c8452da11a
jimzer <jimi.vaubien@protonmail.com>
2023-06-10 00:21:14 +0200

    Fix adapter

--- monorepo/apps/webapp/svelte.config.js
+++ monorepo/apps/webapp/svelte.config.js
@@ -1,13 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	
+
 	vitePlugin: {
-		inspector: true,   
+		inspector: true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
